f272328702ccff6ede9882d09ac69c091b05f74f
Ben Bolker <bbolker@gmail.com>
2019-06-19 00:47:15 -0400

    basic TMB works now

--- simple/simple_occupancy.cpp
+++ simple/simple_occupancy.cpp
@@ -1,49 +1,55 @@
 #include <TMB.hpp>
 
+// logspace_add computes log(exp(logx)+exp(logy))
+// https://kaskr.github.io/adcomp/group__special__functions.html#ga2d5a07f1202dc628ba18ee9a2af8b9cd
+// -log(exp(0) + exp(-log(p/(1-p))) = -log (1 + (1-p)/p) =
+// -log (1/p) = log(p)
+// convert logit(p) to log(p)
+#define invlogit_log(X)    (-logspace_add( Type(0), -X))
+// convert logit(p) to log(1-p)
+#define invlogit_log1mp(X) (-logspace_add( Type(0),  X))
+
 template<class Type>
 Type objective_function<Type>::operator() () {
   
   // parameters
   PARAMETER_VECTOR(beta_p);
-  PARAMETER_VECTOR(beta_psi);
+  PARAMETER_VECTOR(beta_phi);
   
   // data
   DATA_VECTOR(obs);      // observations (not integer??)
   DATA_IVECTOR(site);     // sites corresponding to obs
   DATA_IVECTOR(site_tot); // total obs per site (FIXME: compute this up-front in TMB?)
-  DATA_MATRIX(Xpsi);      // occupancy model matrix (Ns x length(beta_psi))
+  DATA_IVECTOR(site_ind); // site indices (includes 0 and N)
+  DATA_MATRIX(Xphi);      // occupancy model matrix (Ns x length(beta_phi))
   DATA_MATRIX(Xp);        // detection model matrix (N  x length(beta_p
 
-  
-  int N = obs.size();     // total obs
+  int Ns = site_ind.size()-1; // nb sites
 
-  vector<Type> eta_psi = Xpsi * beta_psi;
+  // linear predictors for occupancy and detection
+  vector<Type> eta_phi = Xphi * beta_phi;
   vector<Type> eta_p = Xp * beta_p;
 
-  Type s1, tmp_loglik, nll=0;
+  Type sitesum, nll=0;
 
-  // likelihood (mostly stolen from glmmTMB.cpp)
-  for (int i = 0; i < N; i++) {
-    // dbinom_robust takes logit_prob rather than prob ...
-    tmp_loglik = dbinom_robust(obs(i), Type(1), eta_p(i), true);
-
-    Type logit_pz = eta_psi(site(i)) ;
-
-    // logspace_add computes log(exp(logx)+exp(logy))
-    // https://kaskr.github.io/adcomp/group__special__functions.html#ga2d5a07f1202dc628ba18ee9a2af8b9cd
-
-    Type log_pz   = -logspace_add( Type(0) , -logit_pz );
-    Type log_1mpz = -logspace_add( Type(0) ,  logit_pz );
-    
-    if (site_tot(site(i)) == Type(0)) {
-      //  equivalent: pz = invlogit(logit_pz);
-      //     log( pz(i) + (1.0 - pz(i)) * exp(tmp_loglik) );
-      tmp_loglik = logspace_add( log_pz, log_1mpz + tmp_loglik );
-    } else {
-      // equivalent: log( 1.0 - pz(i))
-      tmp_loglik += log_1mpz ;
+  for (int i = 0; i < Ns; i++) { // loop over sites
+    // FIXME: more vectorized?
+    sitesum=0.0;
+    // conditional detection prob for all sites
+    // (obs(j) could be all-zero)
+    for (int j = site_ind(i); j < site_ind(i+1); j++) {
+      sitesum += dbinom_robust(obs(j), Type(1), eta_p(j), true);
     }
-    nll -= tmp_loglik;
+    if (site_tot(i) > 0) {
+      // non-empty site: log(p_phi(i)) + sum(log(binom detect))
+      nll -= (invlogit_log(eta_phi(i)) + sitesum);
+    } else {
+      // empty site:
+      //  unocc = log(1-p_phi(i))
+      //  undet = log(p_phi(i)) + sum(log(binom detect)) [all zero]
+      nll -= logspace_add(invlogit_log1mp(eta_phi(i)),
+			  invlogit_log(eta_phi(i)) + sitesum);
+    }
   }
   return nll;
 }
